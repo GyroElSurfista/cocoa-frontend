5ef28ec6626dbb7c4244bb10c7137b74d7a50123
Nahuel <nahueltorrezg@gmail.com>
2024-10-01 01:47:54 -0400

    fix: React import never read

--- src/pages/EntregablePage/EntregablePage.tsx
+++ src/pages/EntregablePage/EntregablePage.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import { useLocation, NavLink } from 'react-router-dom'
 import NewEntregableModal from './Components/NewEntregableModal'
 import EntregableAccordion from './Components/EntregableAccordion'
