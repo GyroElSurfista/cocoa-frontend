34bd26716b9493db2362ccb74703c4d40061b5c9
Wimpi18 <winsoromar12@gmail.com>
2024-11-26 23:09:56 -0400

    feat: add date in axios interceptor

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -5,6 +5,20 @@ export const axiosInstance = axios.create({
   baseURL: BASE_URL,
 })
 
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const currentDate = new Date().toISOString() // Obtener la fecha actual en formato ISO 8601
+
+    config.headers = config.headers || {} // Asegurarse de que los headers existan
+    ;(config.headers as Record<string, string>)['X-Current-Date'] = currentDate
+
+    return config
+  },
+  (error) => {
+    return Promise.reject(error)
+  }
+)
+
 export const axiosPrivate = axios.create({
   baseURL: BASE_URL,
   headers: { 'Content-Type': 'application/json' },
