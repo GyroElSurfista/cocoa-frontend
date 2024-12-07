c5e10f337a9035867288a15b1a8f48226b82da19
Mariana Vallejos <marivalleandrea@gmail.com>
2024-12-07 12:03:48 -0400

    Add semester selector

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,78 +1,86 @@
-import { DatePicker, LocalizationProvider } from '@mui/x-date-pickers'
 import logo from '../assets/Logo Project.png'
-import dayjs, { Dayjs } from 'dayjs'
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
 import { useEffect, useState } from 'react'
-import { getSemester, Semester } from '../services/semester.service'
-import { useNavigate } from 'react-router-dom'
-import { Tooltip, Typography } from '@mui/material'
+import { getSemesters, Semester } from '../services/semester.service'
+import { FormControl, InputLabel, MenuItem, Select, Tooltip, Typography, SelectChangeEvent } from '@mui/material'
 import InfoIcon from '@mui/icons-material/Info'
 import { formatDateToDMY } from '../utils/formatDate'
 
 const NavBar = (): JSX.Element => {
-  const [value, setValue] = useState<Dayjs>(dayjs(localStorage.getItem('date') ?? '2024-08-12')) // Si es null entonces le pasamos la fecha inicial
-  const [semester, setSemester] = useState<Semester>()
-  const navigate = useNavigate()
+  const [selectedSemester, setSelectedSemester] = useState<string | null>(localStorage.getItem('id-semester'))
+  const [semesters, setSemesters] = useState<Array<Semester>>([])
+
+  const handleChange = (event: SelectChangeEvent<string>) => {
+    const semesterId = event.target.value
+    setSelectedSemester(semesterId)
+    localStorage.setItem('id-semester', semesterId)
+  }
 
   useEffect(() => {
     const fetchSemester = async () => {
-      const response = await getSemester()
-      const semesterData: Semester = response.data
+      try {
+        const response = await getSemesters()
+        console.log('semester', response.data)
+        const semesterData: Semester = response.data
 
-      setSemester(semesterData)
+        // ToDO: ep to get all semesters
+        setSemesters([semesterData])
+
+        // Si no hay un semestre seleccionado, seleccionar el primero por defecto
+        if (!localStorage.getItem('id-semester') && semesterData.identificador) {
+          const defaultSemesterId = semesterData.identificador.toString()
+          setSelectedSemester(defaultSemesterId)
+          localStorage.setItem('id-semester', defaultSemesterId)
+        }
+      } catch (error) {
+        console.error('Error fetching semesters:', error)
+      }
     }
 
     fetchSemester()
   }, [])
 
-  function disablePrevDates(date: Dayjs) {
-    return (currentDate: Dayjs) => {
-      if (!semester) return true // Mientras no cargue el semestre, deshabilitar todas las fechas
-
-      const semesterStart = dayjs(date.toISOString())
-      const semesterEnd = dayjs(semester.fechaEvaluFin)
-      return currentDate.isBefore(semesterStart, 'day') || currentDate.isAfter(semesterEnd, 'day')
-    }
-  }
+  const currentSemester = semesters.find((semester) => semester.identificador.toString() === selectedSemester)
 
   return (
     <div className="w-full h-20 bg-[#e0e3ff] shadow">
       <div className="flex h-full py-4 sm:mx-4 pl-10 items-center justify-between">
         <img src={logo} className="h-10 sm:h-full" alt="Logo Proyecto Cocoa" />
-        <div className="flex gap-2">
-          <LocalizationProvider dateAdapter={AdapterDayjs}>
-            <Tooltip
-              title={
-                <div>
-                  <Typography variant="body2">
-                    Planificación: {`${formatDateToDMY(semester?.fechaPlaniInici)} - ${formatDateToDMY(semester?.fechaPlaniFin)}`}
-                  </Typography>
-                  <Typography variant="body2">
-                    Desarrollo: {`${formatDateToDMY(semester?.fechaDesaInici)} - ${formatDateToDMY(semester?.fechaDesaFin)}`}
-                  </Typography>
-                  <Typography variant="body2">
-                    Evaluación: {`${formatDateToDMY(semester?.fechaEvaluInici)} - ${formatDateToDMY(semester?.fechaEvaluFin)}`}
-                  </Typography>
-                </div>
-              }
-              placement="left"
+        <div className="flex gap-2 items-center">
+          <Tooltip
+            title={
+              <div>
+                <Typography variant="body2">
+                  Planificación:{' '}
+                  {`${formatDateToDMY(currentSemester?.fechaPlaniInici)} - ${formatDateToDMY(currentSemester?.fechaPlaniFin)}`}
+                </Typography>
+                <Typography variant="body2">
+                  Desarrollo: {`${formatDateToDMY(currentSemester?.fechaDesaInici)} - ${formatDateToDMY(currentSemester?.fechaDesaFin)}`}
+                </Typography>
+                <Typography variant="body2">
+                  Evaluación: {`${formatDateToDMY(currentSemester?.fechaEvaluInici)} - ${formatDateToDMY(currentSemester?.fechaEvaluFin)}`}
+                </Typography>
+              </div>
+            }
+            placement="left"
+          >
+            <InfoIcon />
+          </Tooltip>
+          <FormControl variant="standard" sx={{ m: 1, minWidth: 160 }}>
+            <InputLabel id="semester-select-label">Selecciona el semestre</InputLabel>
+            <Select
+              labelId="semester-select-label"
+              id="semester-select"
+              value={selectedSemester || ''}
+              onChange={handleChange}
+              label="Selecciona el semestre"
             >
-              <InfoIcon />
-            </Tooltip>
-            <DatePicker
-              value={dayjs(value)}
-              onChange={(value) => {
-                if (value) {
-                  setValue(value)
-                  localStorage.setItem('date', value.toISOString().split('T')[0])
-                  navigate('/')
-                }
-              }}
-              label="Fecha en la que te encuentras actualmente"
-              format="DD/MM/YYYY"
-              shouldDisableDate={disablePrevDates(value)}
-            />
-          </LocalizationProvider>
+              {semesters.map((semester) => (
+                <MenuItem value={semester.identificador.toString()} key={semester.identificador}>
+                  {semester.nombre}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
         </div>
       </div>
     </div>
